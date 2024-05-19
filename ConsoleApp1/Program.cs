64ba45d464623232fe490c63e3391523626b3a85
Gabriel <Gabriel@GABRIEL>
2024-05-19 12:52:13 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+Queue<int> intQueue = new(5);
+
+try
+{
+    intQueue.Enqueue(10);
+    intQueue.Enqueue(11);
+    intQueue.Enqueue(12);
+ 
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("Error while enqueueing: " + ex.Message);
+}
+
+Console.WriteLine("Count of elements in intQueue: " + intQueue.Count);
+
+Queue<string> stringQueue = new(3);
+    stringQueue.Enqueue("Gas");
+    stringQueue.Enqueue("Diesel");
+    stringQueue.Enqueue("Electricity");
+Console.WriteLine("Count of elements in stringQueue: " + stringQueue.Count);
+
+
+
+try
+{
+    Console.WriteLine("Dequeued elements:");
+    Console.WriteLine(intQueue.Dequeue());
+    Console.WriteLine(intQueue.Dequeue());
+
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("Error while dequeueing: " + ex.Message);
+}
+
+Console.WriteLine("Count of elements in intQueue after dequeueing: " + intQueue.Count);
---
+++ ConsoleApp1/Queue.cs
@@ -0,0 +1,40 @@
+﻿public class Queue<T>
+{
+    private T[] elements;
+    private int front;
+    private int rear;
+    private int capacity;
+
+    public Queue(int capacity)
+    {
+        this.capacity = capacity;
+        elements = new T[capacity];
+        front = 0;
+        rear = -1;
+    }
+
+    public void Enqueue(T item)
+    {
+        if (rear == capacity - 1)
+        {
+            throw new InvalidOperationException("Queue is full");
+        }
+        elements[++rear] = item;
+    }
+
+    public T Dequeue()
+    {
+        if (front > rear)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        T item = elements[front++];
+        return item;
+    }
+
+    public int Count
+    {
+        get { return rear - front + 1; }
+    }
+
+}
